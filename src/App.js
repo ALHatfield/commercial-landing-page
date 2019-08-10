fef72b8854e02bf272078a9d8d503d0c007d9dd7
Andrew Hatfield <andrew8412@gmail.com>
2019-08-10 12:55:11 -0400

    Code base

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+
+    render() {
+        return(
+            <>
+                <header class="header under-construction"> 
+                    <div class="logo-box under-construction">
+                        <img src="" alt="" class="logo" />
+                    </div>
+                    <div class="text-box under-construction">
+                        <h1 class="header-primary">
+                            <span class="header-primary-main">Indoors</span>
+                            <span class="header-primary-sub">why go outside?</span>
+                        </h1>
+                    </div>
+                </header>
+            </>
+        )
+    }
+}
+
