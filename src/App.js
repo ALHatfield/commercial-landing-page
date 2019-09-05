f42e6ee31af64573417773ad869729561aed820d
Andrew <andrew8412@gmail.com>
2019-09-05 18:59:06 -0400

    added proper banner css style for banner title

--- src/App.js
+++ src/App.js
@@ -6,16 +6,15 @@ export default class App extends Component {
     render() {
         return(
             <>
-                <header className="header under-construction"> 
+                <header className="header"> 
                     <div className="logo-box under-construction">
                         <img src={Brand_Logo} alt="image" className="logo" />
                     </div>
-                    <div className="text-box under-construction">
+                    <div className="header__text-banner under-construction">
                         <h1 className="header-primary">
                             <span className="header-primary-main">Indoors</span>
                             <span className="header-primary-sub">why go outside</span>
                         </h1>
-
                         <a href="#" class="btn btn-white">Discover Sleep</a>
                     </div>
                 </header>
