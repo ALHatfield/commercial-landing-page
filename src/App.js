229dda47634bdb0b879a45201dad4d7a72684c6a
Andrew <andrew8412@gmail.com>
2019-09-05 14:56:30 -0400

    reverted scss to css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import Brand_Logo_195x100px from '../dist/vender/img/logo-white.png'
+import Brand_Logo from '../dist/vender/img/logo-white.png'
 
 export default class App extends Component {
 
@@ -8,13 +8,15 @@ export default class App extends Component {
             <>
                 <header className="header under-construction"> 
                     <div className="logo-box under-construction">
-                        <img src={Brand_Logo_195x100px} alt="image" className="logo" />
+                        <img src={Brand_Logo} alt="image" className="logo" />
                     </div>
                     <div className="text-box under-construction">
                         <h1 className="header-primary">
                             <span className="header-primary-main">Indoors</span>
-                            <span className="header-primary-sub">why go outside?</span>
+                            <span className="header-primary-sub">why go outside</span>
                         </h1>
+
+                        <a href="#" class="btn btn-white">Discover Sleep</a>
                     </div>
                 </header>
             </>
